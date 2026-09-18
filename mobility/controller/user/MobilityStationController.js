0a2978e1895bceeaa69ce6c61266c2441b95e05c
Shivani Sinha <shivani@shunyaekai.tech>
2026-09-18 17:44:05 +0530

    Added distance of 50km for testing server

--- mobility/controller/user/MobilityStationController.js
+++ mobility/controller/user/MobilityStationController.js
@@ -34,7 +34,7 @@ export const cycleStationList = asyncHandler(async (req, resp) => {
     
     const sortOrder = (sort_by === 'd') ? 'DESC' : 'ASC';
     
-    // query += ` HAVING distance <= 10`;
+    query += ` HAVING distance <= 50`;
     query += ` ORDER BY distance ${sortOrder} LIMIT ${start}, ${limit}`;
     const [stations] = await db.execute(query, queryParams);
     
@@ -160,6 +160,7 @@ export const nearByStaion = asyncHandler(async (req, resp) => {
         LEFT JOIN  cycle_list cl ON cl.station_id = msl.station_id 
         GROUP BY msl.station_name, msl.latitude, msl.longitude
         HAVING  cycle_count > 0
+        AND distance <= 50
         ORDER BY distance ASC `, [ latitude, longitude, latitude]
     );
     return resp.json({
@@ -195,7 +196,7 @@ export const nearByStaionDetails = asyncHandler(async (req, resp) => {
             mobility_station_list msl
         LEFT JOIN  cycle_list cl ON cl.station_id = msl.station_id 
         WHERE msl.station_id=? GROUP BY msl.station_name, msl.latitude, msl.longitude
-        HAVING cycle_count > 0 ORDER BY distance ASC `, [ latitude, longitude, latitude, station_id ] 
+        HAVING cycle_count > 0 AND distance <= 50 ORDER BY distance ASC `, [ latitude, longitude, latitude, station_id ] 
     );
     const data = { 
         station,
